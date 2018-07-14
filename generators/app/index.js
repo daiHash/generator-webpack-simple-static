2e02af5f1270acb84c399babb887b314ba2a2b68
Daichi Hashimura <d-hashimura@kakusin.co.jp>
2018-07-14 18:49:55 +0900

    Add comments logs

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,13 +3,24 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const path = require('path');
+const ROOT_DIRECTORY = process
+  .cwd()
+  .split(path.sep)
+  .pop();
 
 module.exports = class extends Generator {
   // Configurations will be loaded here.
   prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(`Welcome to the delightful ${chalk.red('webpack-simple static')} generator!`)
+      yosay(
+        `${chalk.hex('#1395ba')(
+          `Welcome to the fucking amazing and awesome \n${chalk.hex('#EFD460')(
+            'webpack-simple static'
+          )} generator!`
+        )}`
+      )
     );
 
     const prompts = [
@@ -18,13 +29,17 @@ module.exports = class extends Generator {
         name: 'name',
         message: 'Your project name',
         // Defaults to the project's folder name if the input is skipped
-        default: this.appname
+        default: ROOT_DIRECTORY
       }
     ];
     return this.prompt(prompts).then(answer => {
       // To access props later use this.props.someAnswer;
       this.props = answer;
-      this.destinationRoot(answer.name);
+      if (answer.name === this.appname) {
+        this.destinationRoot(answer.default);
+      } else {
+        this.destinationRoot(answer.name);
+      }
     });
   }
 
@@ -36,18 +51,54 @@ module.exports = class extends Generator {
         name: this.props.name
       }
     );
-    this.fs.copyTpl(
+    this.fs.copy(
       this.templatePath('webpack.config.js'),
-      this.destinationPath('webpack.config.js'),
-      {
-        name: this.props.name
-      }
+      this.destinationPath('webpack.config.js')
     );
     this.fs.copy(this.templatePath('_babelrc'), this.destinationPath('.babelrc'));
     this.fs.copy(this.templatePath('src'), this.destinationPath('src'));
   }
 
   install() {
-    this.npmInstall();
+    this.npmInstall().then(() => {
+      if (this.props.name === ROOT_DIRECTORY) {
+        this.log(
+          yosay(
+            `${chalk.hex('#1395ba')(
+              `Thank you for using ${chalk.hex('#EFD460')(
+                'webpack-simple static'
+              )} generator template!`
+            )}`
+          ),
+          `\n - Run ${chalk.hex('##f16c20')('npm run dev')} to start the local server`,
+          `${chalk.hex('#EFD460')(
+            '\nIt is not the best webpack setup, but it is good enough to get started. \nI want to keep improving this template, \nso feel free to send any suggestions for improvements to my Github repo:'
+          )}`,
+          `${chalk.hex('#EFD460')(
+            '\nhttps://github.com/daiHash/generator-webpack-simple-static.'
+          )}`,
+          `${chalk.hex('#EFD460')('\nThanks a lot! Enjoy it!')}`
+        );
+      } else {
+        this.log(
+          yosay(
+            `${chalk.hex('#1395ba')(
+              `Thank you for using ${chalk.hex('#EFD460')(
+                'webpack-simple static'
+              )} generator template!`
+            )}`
+          ),
+          `\n - cd ${chalk.hex('##f16c20')(`${this.props.name}`)}`,
+          `\n - run ${chalk.hex('##f16c20')('npm run dev')} to start the local server.`,
+          `${chalk.hex('#EFD460')(
+            '\nIt is not the best webpack setup, but it is good enough to get started. \nI want to keep improving this template, \nso feel free to send any suggestions for improvements to my Github repo:'
+          )}`,
+          `${chalk.hex('#EFD460')(
+            '\nhttps://github.com/daiHash/generator-webpack-simple-static.'
+          )}`,
+          `${chalk.hex('#EFD460')('\nThanks a lot! Enjoy it!')}`
+        );
+      }
+    });
   }
 };
